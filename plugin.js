faaa596f66b86e7deddeb31a037a99481dd4a15f
Callum Forsyth <cf51@hw.ac.uk>
2022-01-26 15:20:02 +0000

    Adding reference

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 
 
-// 
+// https://developer.chrome.com/docs/extensions/mv3/getstarted/
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
