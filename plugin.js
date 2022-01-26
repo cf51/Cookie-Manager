0a1d70bab4968ad41d252c797efa1c8078c61199
Callum Forsyth <cf51@hw.ac.uk>
2022-01-26 15:38:31 +0000

    references

--- plugin.js
+++ plugin.js
@@ -2,6 +2,7 @@
 
 // https://developer.chrome.com/docs/extensions/mv3/getstarted/
 // https://dev.to/aurelkurtula/basics-of-chrome-extensions-development-part-two-82p 
+// chrome://extensions/
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
