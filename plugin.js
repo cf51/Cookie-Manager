b990e8d175b9d9bf0d0585a6624984b4ef0ee9d7
Callum Forsyth <cf51@hw.ac.uk>
2022-02-08 15:39:15 +0000

    .

--- plugin.js
+++ plugin.js
@@ -3,6 +3,10 @@
 // https://developer.chrome.com/docs/extensions/mv3/getstarted/
 // https://dev.to/aurelkurtula/basics-of-chrome-extensions-development-part-two-82p 
 // chrome://extensions/
+
+function fun(){
+  alert("ALERT");
+}
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
