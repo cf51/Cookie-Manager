d2f050c82f553b9813b71bcd7bc7d869eb501bd8
Callum Forsyth <cf51@hw.ac.uk>
2022-02-08 15:57:07 +0000

    Set & get cookie function

--- plugin.js
+++ plugin.js
@@ -6,12 +6,36 @@
 
 
 // This functio should create a cookie when a vistor comes onto the web page
-function setCookie(name, vlaue, days){
+function setCookie(cookieName, vlaue, days){
   const d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   let expires = "expires=" + d.toUTCString();
-  document.cookie = name + "=" + ";" + expires + ";path=/";
+  document.cookie = cookieName + "=" + ";" + expires + ";path=/";
 }
+
+// This function should get the cookie that has been created above 
+function getCookie(name){
+  let name = cookieName + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' '){
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      returnc.substring(name.length,c.length);
+    }
+  }
+  return"";
+}
+
+// This function will check if a cookie is set
+function checkCookie(){}
+
+
+
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
