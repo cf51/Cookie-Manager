44991ae89b3eadda445bfaf622e44de3644f5bcf
Callum Forsyth <cf51@hw.ac.uk>
2022-02-15 12:31:59 +0000

    Removing code from the website

--- plugin.js
+++ plugin.js
@@ -4,14 +4,6 @@
 // https://dev.to/aurelkurtula/basics-of-chrome-extensions-development-part-two-82p 
 // chrome://extensions/
 
-
-chrome.cookies.getAll({domain: ".mydomain.com"}, function(cookies){
-  for(var i=0; i<cookies.length; i++){
-    console.log(cookies[i]);
-    chrome.cookies.remove({url: "https://" + cookies[i].domain + cookies[i].path, name: cookies[i].name}); 
-  }
-});
-
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
