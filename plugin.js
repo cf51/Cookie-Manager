2210f320473aa3f92900b8d88b6186ac28001836
Callum Forsyth <cf51@hw.ac.uk>
2022-02-15 12:35:44 +0000

    Changing content of listener event

--- plugin.js
+++ plugin.js
@@ -4,8 +4,10 @@
 // https://dev.to/aurelkurtula/basics-of-chrome-extensions-development-part-two-82p 
 // chrome://extensions/
 
+
+// Listening event 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+    console.log('Console:', `color: ${color}`);
   });
  
