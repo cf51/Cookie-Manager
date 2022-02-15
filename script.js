c88372b84ee3d4f7742e5ba1c1d86b5cac78e888
Callum Forsyth <cf51@hw.ac.uk>
2022-02-15 13:57:06 +0000

    IT WORKS

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-document.body.style.backgroundColor = 'orange';
+//document.getElementById("lpc-button").style.backgroundColor = "#ff0000";
+//console.log("This has worked 1"); 
+//document.getElementById("myP").style.color = "magenta";
+//document.getElementById("myP2").style.color = "blue";
+//document.getElementById("myDiv").style.color = "lightblue";
+
+
+
+//document.getElementById("lpc-button").click();
+
+//document.getElementsByClassName("a.a8c-cookie-banner-accept-all-button").click();
+
+// ACCEPT ALL
+//document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-simple-options > a.a8c-cookie-banner-accept-all-button").click();
+console.log("This has worked");
+
+// ANALYTICS OFF
+document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-simple-options > a.a8c-cookie-banner-customize-button").click();
