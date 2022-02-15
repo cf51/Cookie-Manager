21a7b67a5f88bf50192ea0cf107b847e0ca920ee
Callum Forsyth <cf51@hw.ac.uk>
2022-02-15 14:03:35 +0000

    Adding delay functionality (Testing)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 //document.getElementById("myP2").style.color = "blue";
 //document.getElementById("myDiv").style.color = "lightblue";
 
-
+var delay = 5000; // 5 second delay
 
 //document.getElementById("lpc-button").click();
 
@@ -16,3 +16,7 @@ console.log("This has worked");
 
 // ANALYTICS OFF
 document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-simple-options > a.a8c-cookie-banner-customize-button").click();
+setTimeout(function(){
+    document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-options-selection > p:nth-child(3) > input").click();
+    console.log("The delay has worked"); 
+}, delay);
