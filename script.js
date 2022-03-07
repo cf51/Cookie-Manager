05bb75be50d72c2f4beebf9025b5e80669388a5c
Callum Forsyth <cf51@hw.ac.uk>
2022-03-07 13:35:14 +0000

    Bootstrap

--- script.js
+++ script.js
@@ -4,7 +4,10 @@
 //document.getElementById("myP2").style.color = "blue";
 //document.getElementById("myDiv").style.color = "lightblue";
 
-var delay = 3000; // 5 second delay
+var delay = 3000; // 3 second delay
+var delay2 = 5000; 
+var delay3 = 7000; 
+
 
 //document.getElementById("lpc-button").click();
 
@@ -13,10 +16,11 @@ var delay = 3000; // 5 second delay
 /*
 // ACCEPT ALL  (Advertising ON, Analytics ON)
 setTimeout(function(){
-    //console.log("Clicking the accept all button");
-    //document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-simple-options > a.a8c-cookie-banner-accept-all-button").click();
+    console.log("Clicking the accept all button");
+    document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-simple-options > a.a8c-cookie-banner-accept-all-button").click();
 }, delay);
 
+/*
 // ANALYTICS OFF (Essential ONLY)
 setTimeout(function(){
     console.log("Clicking the customisation button");
@@ -46,34 +50,25 @@ setTimeout(function(){
     console.log("Clicking the accept selection button");
     document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-options-selection > a").click();
 }, delay);
-*/
 
+*/ 
+// DEMO MODE
 // TESTING (Will select and change the colour of the button)
 setTimeout(function(){
     //Clicking the customisation button
     document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-simple-options > a.a8c-cookie-banner-customize-button").click();
+}, delay);
 
-    //Changing the analytics button to red
-    document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-options-selection > p:nth-child(3) > strong").style.color = "#ff0000";
+setTimeout(function(){
+     //Changing the advertising button to red
+     //document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-options-selection > p:nth-child(4) > strong").style.color = "#ff0000";
+     //Clicking the advertising button
+     document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-options-selection > p:nth-child(4) > input").click();
+}, delay2);
 
-    //Changing the advertising button to red
-    document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-options-selection > p:nth-child(4) > strong").style.color = "#ff0000";
-
-    //Clicking the advertising button
-    document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-options-selection > p:nth-child(4) > input").click();
-
+setTimeout(function(){
     //Changing the button to red
     document.querySelector("#wpcom-home > form > div.a8c-cookie-banner-options-selection > a").style.backgroundColor = "#ff0000";
-}, delay);
+}, delay3);
  
-
-
-
-
-
-
-
-
-
-
 
