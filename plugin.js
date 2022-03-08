c6bb084d5440cca860cc790e494ca5297fb4eb48
Callum Forsyth <cf51@hw.ac.uk>
2022-03-08 14:06:08 +0000

    Adding check button functionalty

--- plugin.js
+++ plugin.js
@@ -4,19 +4,24 @@
 // https://dev.to/aurelkurtula/basics-of-chrome-extensions-development-part-two-82p 
 // chrome://extensions/
 
-function myfunction(){
-  alert("THIS HAS WORKED"); 
-  console.log("worked"); 
-}
+//This script deals with the functionality within the extnesions user interface
 
-if 
-
-
+let checked = 0;
+console.log(checked)
 // Listening event 
-/*
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Console:', `color: ${color}`);
   });
 
-*/ 
+  document.addEventListener('DOMContentLoaded', function() {
+    var checkButton = document.getElementById('advertisingCheck');
+    checkButton.addEventListener('change', function() {
+      if (checkButton.checked){
+        alert("BUTTON HAS BEEN CHECKED"); 
+        checked = checked + 1;  
+        console.log(checked);
+      }
+    }, false);
+  }, false);
+
